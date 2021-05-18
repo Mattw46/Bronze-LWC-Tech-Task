481e1031481a4ad9824ef4138a1d2ef1797a285c
Mattw46 <mattw76@live.com.au>
2021-05-18 11:03:51 +1000

    final draft

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -5,14 +5,10 @@ export default class ChildComponent extends LightningElement {
 
    inputitemselect() {
       // propagate event to parent
-      console.log('inputitemselect fired');
+
    }
 
    fireSelectEvent(event) {
-      console.log('fireSelectEvent fired');
-      //console.log(this.inputItem.Key);
-      //this.dispatchEvent(new CustomEvent(this.inputItem.Key));
-      console.log('Key is: ' + this.inputItem.Key);
       const selectedEvent = new CustomEvent('selected', { detail: this.inputItem.Key});
       this.dispatchEvent(selectedEvent);
    }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -7,19 +7,11 @@ export default class ParentComponent extends LightningElement {
    newInput=null;
 
    addInputToList() {
-      console.log("button clicked");
-      
       this.newInput = this.template.querySelector("lightning-input").value;
       this.list.push({Key: this.newInput, Value: this.newInput});
-
-      for (var current in this.list) {
-         console.log(this.list[current]);
-      }
    }
 
    handleSelect(event) {
-      // handle child event
-      console.log('handle select called: ' + event.detail);
       this.input = event.detail;
       this.selected = true;
    }
